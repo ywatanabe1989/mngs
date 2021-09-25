f05d4eccbd7c46c03b23d6f095c7d9f0ad048454
Yusuke Watanabe <wyusuuke@gmail.com>
2021-09-25 10:53:10 +0900

    fixed

--- src/mngs/stats/_nocorrelation_test.py
+++ src/mngs/stats/_nocorrelation_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from scipy import stats
 
 
 def calc_partial_corrcoef(x, y, z):
@@ -22,6 +23,6 @@ def nocorrelation_test(x, y, z=None, alpha=0.05):
     df = n - 2
     # t = np.abs(np.array(r)) * np.sqrt((df) / (1 - np.array(r)**2))
     t = np.abs(r) * np.sqrt((df) / (1 - r ** 2))
-    t_alpha = stats.t.ppf(1 - alpha / 2, df)
+    # t_alpha = scipy.stats.t.ppf(1 - alpha / 2, df)
     p_value = 2 * (1 - stats.t.cdf(t, df))
     return r, t, p_value
