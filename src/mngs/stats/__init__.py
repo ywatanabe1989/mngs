e3a6bf9d17e8e4f25783e6b5abdd391e46fa152a
Yusuke Watanabe <wyusuuke@gmail.com>
2021-09-25 10:00:26 +0900

    add

--- src/mngs/stats/__init__.py
+++ src/mngs/stats/__init__.py
@@ -4,3 +4,4 @@ from ._brunner_munzel_test import brunner_munzel_test
 from ._multicompair import multicompair
 from ._nocorrelation_test import nocorrelation_test
 from ._calc_partial_corr import calc_partial_corr
+from ._fdr_correction import fdr_correction, fdr_correction_torch
