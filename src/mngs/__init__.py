10d30d54f20000163427f79c06ce75f2a85f8153
Yusuke Watanabe <wyusuuke@gmail.com>
2024-01-29 05:41:08 +0900

    v0.10.2

--- src/mngs/__init__.py
+++ src/mngs/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Time-stamp: "2024-01-28 09:37:16 (ywatanabe)"
+# Time-stamp: "2024-01-29 07:40:50 (ywatanabe)"
 
 from . import dsp
 from . import general
@@ -8,7 +8,7 @@ from . import io, linalg, ml, nn, plt, resource, stats
 from .general.debug import *
 
 __copyright__ = "Copyright (C) 2021 Yusuke Watanabe"
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 __license__ = "GPL3.0"
 __author__ = "ywatanabe1989"
 __author_email__ = "ywata1989@gmail.com"
