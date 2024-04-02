c0b4dc8ef4c112e9ca6cab8642ca797a50ad082c
Yusuke Watanabe <wyusuuke@gmail.com>
2024-04-03 00:25:34 +1100

    update

--- update_package.sh
+++ update_package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf build dist/* src/mngs.egg-info
+# tree src | grep \.py except for .py under __pycache__
+tree src -I __pycache__ > tree.txt
 python3 setup.py sdist bdist_wheel
 twine upload -r pypi dist/*
 
