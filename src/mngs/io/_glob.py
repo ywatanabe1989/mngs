62e08eb76ee631edd332ee8afa05e10d2ea80a3a
OpenDevin <opendevin@all-hands.dev>
2024-08-29 00:08:52 +0000

    Update glob function docstring in io submodule

--- src/mngs/io/_glob.py
+++ src/mngs/io/_glob.py
@@ -9,8 +9,34 @@ from glob import glob as _glob
 
 
 def glob(expression):
+    """
+    Perform a glob operation with natural sorting and extended pattern support.
+
+    This function extends the standard glob functionality by adding natural sorting
+    and support for curly brace expansion in the glob pattern.
+
+    Parameters:
+    -----------
+    expression : str
+        The glob pattern to match against file paths. Supports standard glob syntax
+        and curly brace expansion (e.g., 'dir/{a,b}/*.txt').
+
+    Returns:
+    --------
+    list
+        A naturally sorted list of file paths that match the given expression.
+
+    Examples:
+    ---------
+    >>> glob('data/*.txt')
+    ['data/file1.txt', 'data/file2.txt', 'data/file10.txt']
+
+    >>> glob('data/{a,b}/*.txt')
+    ['data/a/file1.txt', 'data/a/file2.txt', 'data/b/file1.txt']
+    """
     glob_pattern = re.sub(r"{[^}]*}", "*", expression)
     try:
         return natsorted(_glob(eval(glob_pattern)))
     except:
         return natsorted(_glob(glob_pattern))
+
