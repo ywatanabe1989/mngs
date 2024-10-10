a38edd77df7bf3e72e36040c24e3fee0bdc321e9
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2024-10-11 08:02:42 +1100

    .

--- src/mngs/__init__.py
+++ src/mngs/__init__.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
-# Time-stamp: "2024-10-08 20:49:56 (ywatanabe)"
+# Time-stamp: "2024-10-11 08:02:16 (ywatanabe)"
 
+# from . import io, path
 
-from . import io, path
+# # Core modules
+# try:
+#     from . import gen, general
+#     from ._sh import sh
+# except ImportError as e:
+#     print(f"Warning: Failed to import some core modules. Error: {e}")
 
-None  # to keep the importing order
-from . import gen, general
-from ._sh import sh
+# Additional modules
+additional_modules = ['io', 'path', 'gen', 'general', 'sh', 'ai', 'dsp', 'gists', 'linalg', 'ml', 'nn', 'os', 'pd', 'plt', 'stats', 'torch', 'tex', 'typing']
+for module in additional_modules:
+    try:
+        exec(f"from . import {module}")
+    except ImportError as e:
+        print(f"Warning: Failed to import {module}. Error: {e}")
 
-None
-from . import ai, dsp, gists, linalg, ml, nn, os, pd, plt, stats, torch, tex, typing
+try:
+    from . import res
+except ImportError as e:
+    print(f"Warning: Failed to import res. Error: {e}")
 
-None
-from . import res
-
-None  # to keep the importing order
-from . import web
+try:
+    from . import web
+except ImportError as e:
+    print(f"Warning: Failed to import web. Error: {e}")
 
 __copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
 __version__ = "1.8.0"
@@ -23,3 +34,29 @@ __license__ = "MIT"
 __author__ = "ywatanabe1989"
 __author_email__ = "ywatanabe@alumni.u-tokyo.ac.jp"
 __url__ = "https://github.com/ywatanabe1989/mngs"
+
+# #!/usr/bin/env python3
+# # Time-stamp: "2024-10-08 20:49:56 (ywatanabe)"
+
+
+# from . import io, path
+
+# None  # to keep the importing order
+# from . import gen, general
+# from ._sh import sh
+
+# None
+# from . import ai, dsp, gists, linalg, ml, nn, os, pd, plt, stats, torch, tex, typing
+
+# None
+# from . import res
+
+# None  # to keep the importing order
+# from . import web
+
+# __copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
+# __version__ = "1.8.0"
+# __license__ = "MIT"
+# __author__ = "ywatanabe1989"
+# __author_email__ = "ywatanabe@alumni.u-tokyo.ac.jp"
+# __url__ = "https://github.com/ywatanabe1989/mngs"
