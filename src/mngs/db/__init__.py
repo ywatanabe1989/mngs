d0532c5d45436d5f14bcba7da9b21cdba3307da0
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2024-10-15 00:49:36 +1100

    .

---
+++ src/mngs/db/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2024-10-15 00:35:59 (ywatanabe)"
+# File: 2024-10-15 00:35:40
+
+from ._inspect import inspect, Inspector
--- src/mngs/typing/__init__.py
+++ src/mngs/typing/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2024-10-13 19:04:42 (ywatanabe)"
+# Time-stamp: "2024-10-14 00:28:03 (ywatanabe)"
 # File: 2024-10-07 22:17:30
 
 try:
-    from ._typing import List, Tuple, Dict, Any, Union, Sequence, Literal, Optional, Iterable, ArrayLike
+    from ._typing import List, Tuple, Dict, Any, Union, Sequence, Literal, Optional, Iterable, Generator, ArrayLike
 except ImportError as e:
     pass # print(f"Warning: Failed to import from ._typing.")
 
-# from ._typing import List, Tuple, Dict, Any, Union, Sequence, Literal, Optional, Iterable, ArrayLike
+# from ._typing import List, Tuple, Dict, Any, Union, Sequence, Literal, Optional, Iterable, Generator, ArrayLike
