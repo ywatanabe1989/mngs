ee792bc69581b2f9bfe7684045e917402b445ea0
LLEmacs <llemacs@llemacs.com>
2025-04-27 20:36:38 +1000

    Add and improve test for add_marginal_ax

--- tests/mngs/plt/ax/test__add_marginal_ax.py
+++ tests/mngs/plt/ax/test__add_marginal_ax.py
@@ -1,32 +1,43 @@
-# Source code from: /home/ywatanabe/proj/mngs_dev/src/mngs/plt/ax/_add_marginal_ax.py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Timestamp: "2025-04-27 13:29:20 (ywatanabe)"
+# File: /home/ywatanabe/proj/mngs_repo/tests/mngs/plt/ax/test__add_marginal_ax.py
+# ----------------------------------------
+import os
+__FILE__ = (
+    "./tests/mngs/plt/ax/test__add_marginal_ax.py"
+)
+__DIR__ = os.path.dirname(__FILE__)
+# ----------------------------------------
+# Source code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/ax/_add_marginal_ax.py
 # --------------------------------------------------------------------------------
 # #!./env/bin/python3
 # # -*- coding: utf-8 -*-
 # # Time-stamp: "2024-07-11 06:06:21 (ywatanabe)"
 # # ./src/mngs/plt/ax/_add_marginal_ax.py
-# 
-# 
+#
+#
 # """
 # This script does XYZ.
 # """
-# 
-# 
+#
+#
 # """
 # Imports
 # """
 # import os
 # import re
 # import sys
-# 
+#
 # import matplotlib
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 # import importlib
-# 
+#
 # import mngs
-# 
+#
 # importlib.reload(mngs)
-# 
+#
 # import numpy as np
 # import pandas as pd
 # import torch
@@ -41,56 +52,56 @@
 # from tqdm import tqdm
 # import xarray as xr
 # from mpl_toolkits.axes_grid1 import make_axes_locatable
-# 
+#
 # # sys.path = ["."] + sys.path
 # # from scripts import utils, load
-# 
+#
 # """
 # Warnings
 # """
 # # warnings.simplefilter("ignore", UserWarning)
-# 
-# 
+#
+#
 # """
 # Config
 # """
 # # CONFIG = mngs.gen.load_configs()
-# 
-# 
+#
+#
 # """
 # Functions & Classes
 # """
-# 
-# 
+#
+#
 # # def add_marginal_axes(ax):
 # #     divider = make_axes_locatable(ax)
-# 
+#
 # #     ax_marg_x = divider.append_axes("top", size="20%", pad=0.1)
 # #     ax_marg_x.set_box_aspect(0.2)
-# 
+#
 # #     ax_marg_y = divider.append_axes("right", size="20%", pad=0.1)
 # #     ax_marg_y.set_box_aspect(0.2 ** (-1))
-# 
+#
 # #     return ax_marg_x, ax_marg_y
-# 
-# 
+#
+#
 # def add_marginal_ax(ax, place, size=0.2, pad=0.1):
 #     divider = make_axes_locatable(ax)
-# 
+#
 #     size_perc_str = f"{size*100}%"
 #     if place in ["left", "right"]:
 #         size = 1.0 / size
-# 
+#
 #     ax_marg = divider.append_axes(place, size=size_perc_str, pad=pad)
 #     ax_marg.set_box_aspect(size)
-# 
+#
 #     return ax_marg
-# 
-# 
+#
+#
 # def main():
 #     pass
-# 
-# 
+#
+#
 # if __name__ == "__main__":
 #     # # Argument Parser
 #     # import argparse
@@ -98,51 +109,43 @@
 #     # parser.add_argument('--var', '-v', type=int, default=1, help='')
 #     # parser.add_argument('--flag', '-f', action='store_true', default=False, help='')
 #     # args = parser.parse_args()
-# 
+#
 #     # Main
 #     CONFIG, sys.stdout, sys.stderr, plt, CC = mngs.gen.start(
 #         sys, plt, verbose=False
 #     )
 #     main()
 #     mngs.gen.close(CONFIG, verbose=False, notify=False)
-# 
+#
 # # EOF
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import os
 import sys
+
+#!/usr/bin/env python3
 from pathlib import Path
-import pytest
-import numpy as np
+
+import matplotlib.pyplot as plt
 
 # Add source code to the top of Python path
 project_root = str(Path(__file__).resolve().parents[3])
 if project_root not in sys.path:
     sys.path.insert(0, os.path.join(project_root, "src"))
 
-from mngs.plt.ax._add_marginal_ax import *
+from mngs.plt.ax._add_marginal_ax import add_marginal_ax
 
-class TestMainFunctionality:
-    def setup_method(self):
-        # Setup test fixtures
-        pass
 
-    def teardown_method(self):
-        # Clean up after tests
-        pass
+def test_add_marginal_ax_basic():
+    fig, ax = plt.subplots()
+    # create top marginal
+    ax_top = add_marginal_ax(ax, "top", size=0.3, pad=0.1)
+    assert ax_top is not ax
+    assert ax_top.get_figure() is fig
 
-    def test_basic_functionality(self):
-        # Basic test case
-        raise NotImplementedError("Test not yet implemented")
 
-    def test_edge_cases(self):
-        # Edge case testing
-        raise NotImplementedError("Test not yet implemented")
+def test_add_marginal_ax_left_right():
+    fig, ax = plt.subplots()
+    for place in ("left", "right"):
+        ax_side = add_marginal_ax(ax, place)
+        assert ax_side.get_figure() is fig
 
-    def test_error_handling(self):
-        # Error handling testing
-        raise NotImplementedError("Test not yet implemented")
-
-if __name__ == "__main__":
-    pytest.main([os.path.abspath(__file__)])
+# EOF
