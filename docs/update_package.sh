a74ac7f25b5a532c2d8dd6f9734c1e8cdea604ac
LLEmacs <llemacs@llemacs.com>
2025-04-27 20:36:37 +1000

    Update update_package.sh script

--- docs/update_package.sh
+++ docs/update_package.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Time-stamp: "2024-11-07 18:18:23 (ywatanabe)"
-# File: ./mngs_repo/docs/update_package.sh
+# -*- coding: utf-8 -*-
+# Timestamp: "2025-04-27 12:57:54 (ywatanabe)"
+# File: ./docs/update_package.sh
+
+THIS_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+LOG_PATH="$THIS_DIR/.$(basename $0).log"
+touch "$LOG_PATH" >/dev/null 2>&1
 
 
-rm -rf build dist/* src/mngs.egg-info
-python3 setup.py sdist bdist_wheel
-twine upload -r pypi dist/*
+main() {
+    rm -rf build dist/* src/mngs.egg-info
+    python3 setup.py sdist bdist_wheel
+    twine upload -r pypi dist/*
+}
 
+main | tee "$LOG_PATH"
 
 # EOF
