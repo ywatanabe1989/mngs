bca07b639a8d130204ce17da8958a8f23b3d938a
LLEmacs <llemacs@llemacs.com>
2025-05-03 01:13:06 +1000

    Update imports

--- tests/mngs/plt/ax/_plot/test___init__.py
+++ tests/mngs/plt/ax/_plot/test___init__.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 # --------------------------------------------------------------------------------
 # #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
-# # Timestamp: "2025-05-02 18:27:00 (ywatanabe)"
+# # Timestamp: "2025-05-02 20:12:19 (ywatanabe)"
 # # File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/ax/_plot/__init__.py
 # # ----------------------------------------
 # import os
@@ -21,26 +21,39 @@ if __name__ == "__main__":
 # )
 # __DIR__ = os.path.dirname(__FILE__)
 # # ----------------------------------------
+# # #!/usr/bin/env python3
+# # # -*- coding: utf-8 -*-
+# # # Timestamp: "2025-05-02 20:09:03 (ywatanabe)"
+# # # File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/ax/_plot/__init__.py
+# # # ----------------------------------------
+# # import os
+# # __FILE__ = (
+# #     "./src/mngs/plt/ax/_plot/__init__.py"
+# # )
+# # __DIR__ = os.path.dirname(__FILE__)
+# # # ----------------------------------------
 # 
-# from ._plot_scatter_hist import plot_scatter_hist
-# from ._plot_heatmap import plot_heatmap
-# from ._plot_circular_hist import plot_circular_hist
-# from ._plot_conf_mat import plot_conf_mat
-# from ._plot_cube import plot_cube
-# from ._plot_ecdf import plot_ecdf
-# from ._plot_fillv import plot_fillv
-# from ._plot_half_violin import plot_half_violin
-# from ._plot_image import plot_image
-# from ._plot_joyplot import plot_joyplot
-# from ._plot_raster import plot_raster
-# from ._plot_rectangle import plot_rectangle
-# from ._plot_shaded_line import plot_shaded_line
-# from ._plot_statistical_shaded_line import (
-#     plot_line,
-#     plot_mean_std,
-#     plot_mean_ci,
-#     plot_median_iqr,
-# )
+# # from ._plot_scatter_hist import plot_scatter_hist
+# # from ._plot_heatmap import plot_heatmap
+# # from ._plot_circular_hist import plot_circular_hist
+# # from ._plot_conf_mat import plot_conf_mat
+# # from ._plot_cube import plot_cube
+# # from ._plot_ecdf import plot_ecdf
+# # from ._plot_fillv import plot_fillv
+# # from ._plot_violin import plot_violin
+# # from ._plot_image import plot_image
+# # from ._plot_joyplot import plot_joyplot
+# # from ._plot_raster import plot_raster
+# # from ._plot_rectangle import plot_rectangle
+# # from ._plot_shaded_line import plot_shaded_line
+# # from ._plot_statistical_shaded_line import (
+# #     plot_line,
+# #     plot_mean_std,
+# #     plot_mean_ci,
+# #     plot_median_iqr,
+# # )
+# 
+# # # EOF
 # 
 # # EOF
 # --------------------------------------------------------------------------------
