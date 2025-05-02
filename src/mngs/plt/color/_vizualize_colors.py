c9a444b1916abbafe5cd84f0bf4efd84b03c581b
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:02:03 +1000

    Refactor: Improved clarity and consistency for vizualize_colors module

---
+++ src/mngs/plt/color/_vizualize_colors.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Timestamp: "2025-05-02 12:58:07 (ywatanabe)"
+# File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/colors/_vizualize_colors.py
+# ----------------------------------------
+import os
+__FILE__ = (
+    "./src/mngs/plt/colors/_vizualize_colors.py"
+)
+__DIR__ = os.path.dirname(__FILE__)
+# ----------------------------------------
+
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def vizualize_colors(colors):
+
+    def gen_rand_sample(size=100):
+        x = np.linspace(-1, 1, size)
+        y = np.random.normal(size=size)
+        s = np.random.randn(size)
+        return x, y, s
+
+    from .. import subplots as mngs_plt_subplots
+
+    fig, axes = mngs_plt_subplots(ncols=4)
+
+    for ii, (color_str, rgba) in enumerate(colors.items()):
+        xx, yy, ss = gen_rand_sample()
+
+        # Box color plot
+        axes[0].plot_rectangle(
+            xx=ii, yy=0, width=1, height=1, color=rgba, label=color_str
+        )
+
+        # Line plot
+        axes[1].plot_shaded_line(
+            xx, yy - ss, yy, yy + ss, colors=rgba, label=color_str
+        )
+
+        # Scatter plot
+        axes[2].scatter(xx, yy, color=rgba, label=color_str)
+
+        # KDE plot
+        axes[3].plot_kde(yy, color=rgba, label=color_str)
+
+    for ax in axes.flat:
+        # ax.axis("off")
+        ax.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    import mngs
+
+    # # Argument Parser
+    # import argparse
+    # parser = argparse.ArgumentParser(description='')
+    # parser.add_argument('--var', '-v', type=int, default=1, help='')
+    # parser.add_argument('--flag', '-f', action='store_true', default=False, help='')
+    # args = parser.parse_args()
+    # Main
+    CONFIG, sys.stdout, sys.stderr, plt, CC = mngs.gen.start(
+        sys, plt, verbose=False
+    )
+    main()
+    mngs.gen.close(CONFIG, verbose=False, notify=False)
+
+# EOF
