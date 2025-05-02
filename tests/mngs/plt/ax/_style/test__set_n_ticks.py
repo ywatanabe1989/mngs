1b15243e4c9c524f17c1dd005ebe746fae035647
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:04:06 +1000

    Add test for ax._style._set_n_ticks.py

---
+++ tests/mngs/plt/ax/_style/test__set_n_ticks.py
@@ -0,0 +1,52 @@
+# Add your tests here
+
+if __name__ == "__main__":
+    import os
+
+    import pytest
+
+    pytest.main([os.path.abspath(__file__)])
+
+# --------------------------------------------------------------------------------
+# Start of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/ax/_style/_set_n_ticks.py
+# --------------------------------------------------------------------------------
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# # Timestamp: "2025-04-29 12:02:14 (ywatanabe)"
+# # File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/ax/_set_n_ticks.py
+# # ----------------------------------------
+# import os
+# __FILE__ = (
+#     "./src/mngs/plt/ax/_set_n_ticks.py"
+# )
+# __DIR__ = os.path.dirname(__FILE__)
+# # ----------------------------------------
+# 
+# import matplotlib
+# 
+# 
+# def set_n_ticks(
+#     ax,
+#     n_xticks=4,
+#     n_yticks=4,
+# ):
+#     """
+#     Example:
+#         ax = set_n_ticks(ax)
+#     """
+# 
+#     if n_xticks is not None:
+#         ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(n_xticks))
+# 
+#     if n_yticks is not None:
+#         ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(n_yticks))
+# 
+#     # Force the figure to redraw to reflect changes
+#     ax.figure.canvas.draw()
+# 
+#     return ax
+# 
+# # EOF
+# --------------------------------------------------------------------------------
+# End of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/ax/_style/_set_n_ticks.py
+# --------------------------------------------------------------------------------
