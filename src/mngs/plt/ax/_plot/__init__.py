10cc38e781423232535b01e539a58bae3a6c27b9
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:01:28 +1000

    add plot_heatmap and plot_scatter_hist

--- src/mngs/plt/ax/_plot/__init__.py
+++ src/mngs/plt/ax/_plot/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-01 08:53:03 (ywatanabe)"
+# Timestamp: "2025-05-02 18:27:00 (ywatanabe)"
 # File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/ax/_plot/__init__.py
 # ----------------------------------------
 import os
@@ -10,7 +10,8 @@ __FILE__ = (
 __DIR__ = os.path.dirname(__FILE__)
 # ----------------------------------------
 
-from ._plot_annotated_heatmap import plot_annotated_heatmap
+from ._plot_scatter_hist import plot_scatter_hist
+from ._plot_heatmap import plot_heatmap
 from ._plot_circular_hist import plot_circular_hist
 from ._plot_conf_mat import plot_conf_mat
 from ._plot_cube import plot_cube
@@ -18,7 +19,7 @@ from ._plot_ecdf import plot_ecdf
 from ._plot_fillv import plot_fillv
 from ._plot_half_violin import plot_half_violin
 from ._plot_image import plot_image
-from ._plot_joyplot import plot_vertical_joyplot, plot_horizontal_joyplot
+from ._plot_joyplot import plot_joyplot
 from ._plot_raster import plot_raster
 from ._plot_rectangle import plot_rectangle
 from ._plot_shaded_line import plot_shaded_line
