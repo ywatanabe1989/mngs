a2aed64b36f5cd3722a6bb32f556f29ff9d5e14c
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:04:39 +1000

    Add test for color module init

---
+++ tests/mngs/plt/color/test___init__.py
@@ -0,0 +1,68 @@
+# Add your tests here
+
+if __name__ == "__main__":
+    import os
+
+    import pytest
+
+    pytest.main([os.path.abspath(__file__)])
+
+# --------------------------------------------------------------------------------
+# Start of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/color/__init__.py
+# --------------------------------------------------------------------------------
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# # Timestamp: "2025-05-02 17:56:28 (ywatanabe)"
+# # File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/color/__init__.py
+# # ----------------------------------------
+# import os
+# __FILE__ = (
+#     "./src/mngs/plt/color/__init__.py"
+# )
+# __DIR__ = os.path.dirname(__FILE__)
+# # ----------------------------------------
+# 
+# from ._get_colors_from_cmap import (
+#     get_color_from_cmap,
+#     get_colors_from_cmap,
+#     get_categorical_colors_from_cmap,
+# )
+# from ._interp_colors import interp_colors
+# from ._PARAMS import PARAMS
+# from ._vizualize_colors import vizualize_colors
+# from ._colors import (
+#     # RGB
+#     str2rgb,
+#     str2rgba,
+#     rgb2rgba,
+#     rgba2rgb,
+#     rgba2hex,
+#     cycle_color_rgb,
+#     gradiate_color_rgb,
+#     gradiate_color_rgba,
+#     # BGR
+#     str2bgr,
+#     str2bgra,
+#     bgr2bgra,
+#     bgra2bgr,
+#     bgra2hex,
+#     cycle_color_bgr,
+#     gradiate_color_bgr,
+#     gradiate_color_bgra,
+#     # COMMON
+#     rgb2bgr,
+#     bgr2rgb,
+#     str2hex,
+#     update_alpha,
+#     cycle_color,
+#     gradiate_color,
+#     to_rgb,
+#     to_rgba,
+#     to_hex,
+#     gradiate_color,
+# )
+# 
+# # EOF
+# --------------------------------------------------------------------------------
+# End of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/color/__init__.py
+# --------------------------------------------------------------------------------
