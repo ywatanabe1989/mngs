752710bd1a0518e57b3621734ecfdd68db18d7df
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:01:28 +1000

    Change from adjust to style module

--- src/mngs/plt/ax/__init__.py
+++ src/mngs/plt/ax/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-01 08:52:41 (ywatanabe)"
+# Timestamp: "2025-05-02 18:29:52 (ywatanabe)"
 # File: /home/ywatanabe/proj/mngs_repo/src/mngs/plt/ax/__init__.py
 # ----------------------------------------
 import os
@@ -11,19 +11,22 @@ __DIR__ = os.path.dirname(__FILE__)
 # ----------------------------------------
 
 # Adjust
-from ._adjust._add_marginal_ax import add_marginal_ax
-from ._adjust._add_panel import add_panel, panel
-from ._adjust._extend import extend
-from ._adjust._force_aspect import force_aspect
-from ._adjust._format_label import format_label
-from ._adjust._hide_spines import hide_spines
-from ._adjust._sci_note import sci_note
-from ._adjust._set_n_ticks import set_n_ticks
-from ._adjust._set_size import set_size
-from ._adjust._set_supxyt import set_supxyt
-from ._adjust._set_ticks import set_ticks
-from ._adjust._set_xyt import set_xyt
-from ._adjust._share_axes import (
+from ._style._add_marginal_ax import add_marginal_ax
+from ._style._add_panel import add_panel, panel
+from ._style._extend import extend
+from ._style._force_aspect import force_aspect
+from ._style._format_label import format_label
+from ._style._hide_spines import hide_spines
+from ._style._map_ticks import map_ticks
+from ._style._rotate_labels import rotate_labels
+from ._style._sci_note import sci_note
+from ._style._set_n_ticks import set_n_ticks
+from ._style._set_size import set_size
+from ._style._set_supxyt import set_supxyt
+from ._style._set_ticks import set_ticks
+from ._style._set_xyt import set_xyt
+from ._style._shift import shift
+from ._style._share_axes import (
     get_global_xlim,
     get_global_ylim,
     set_xlims,
@@ -32,27 +35,27 @@ from ._adjust._share_axes import (
     sharexy,
     sharey,
 )
-from ._adjust._shift import shift
-from ._adjust._rotate_labels import rotate_labels
-from ._adjust._map_ticks import map_ticks
 
 # Plot
-from ._plot._plot_image import plot_image
-from ._plot._plot_fillv import plot_fillv
+from ._plot._plot_heatmap import plot_heatmap
 from ._plot._plot_circular_hist import plot_circular_hist
+from ._plot._plot_conf_mat import plot_conf_mat
+from ._plot._plot_cube import plot_cube
 from ._plot._plot_ecdf import plot_ecdf
-from ._plot._plot_joyplot import plot_vertical_joyplot, plot_horizontal_joyplot
+from ._plot._plot_fillv import plot_fillv
+from ._plot._plot_half_violin import plot_half_violin
+from ._plot._plot_image import plot_image
+from ._plot._plot_joyplot import plot_joyplot
+from ._plot._plot_raster import plot_raster
+from ._plot._plot_rectangle import plot_rectangle
+from ._plot._plot_scatter_hist import plot_scatter_hist
+from ._plot._plot_shaded_line import plot_shaded_line
 from ._plot._plot_statistical_shaded_line import (
     plot_line,
     plot_mean_std,
     plot_mean_ci,
     plot_median_iqr,
 )
-from ._plot._plot_shaded_line import plot_shaded_line
-from ._plot._plot_raster import plot_raster
-from ._plot._plot_cube import plot_cube
-from ._plot._plot_rectangle import plot_rectangle
-from ._plot._plot_half_violin import plot_half_violin
 
 
 # ################################################################################
