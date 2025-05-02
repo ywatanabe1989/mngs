ae8c1a67e776e3d30597a7f2ce35e85f6f3b1d03
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:04:08 +1000

    Remove test__extend.py

--- tests/mngs/plt/ax/test__extend.py
+++ tests/mngs/plt/ax/test__extend.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-04-28 16:31:14 (ywatanabe)"
+# Timestamp: "2025-05-02 09:19:08 (ywatanabe)"
 # File: /home/ywatanabe/proj/mngs_repo/tests/mngs/plt/ax/test__extend.py
 # ----------------------------------------
 import os
@@ -9,136 +9,112 @@ __FILE__ = (
 )
 __DIR__ = os.path.dirname(__FILE__)
 # ----------------------------------------
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pytest
-from mngs.plt.ax._extend import extend
+# Time-stamp: "2024-11-03 10:33:13 (ywatanabe)"
+# File: placeholder.py
 
-matplotlib.use("Agg")
+"""
+Functionalities:
+  - Does XYZ
+  - Does XYZ
+  - Does XYZ
+  - Saves XYZ
+
+Dependencies:
+  - scripts:
+    - /path/to/script1
+    - /path/to/script2
+  - packages:
+    - package1
+    - package2
+IO:
+  - input-files:
+    - /path/to/input/file.xxx
+    - /path/to/input/file.xxx
+
+  - output-files:
+    - /path/to/input/file.xxx
+    - /path/to/input/file.xxx
+
+(Remove me: Please fill docstrings above, while keeping the bulette point style, and remove this instruction line)
+"""
+
+"""Imports"""
+import argparse
+
+"""Warnings"""
+# mngs.pd.ignore_SettingWithCopyWarning()
+# warnings.simplefilter("ignore", UserWarning)
+# with warnings.catch_warnings():
+#     warnings.simplefilter("ignore", UserWarning)
+
+"""Parameters"""
+# from mngs.io import load_configs
+# CONFIG = load_configs()
+
+"""Functions & Classes"""
+def main(args):
+    pass
 
 
-class TestMainFunctionality:
-    def setup_method(self):
-        # Setup test fixtures
-        self.fig = plt.figure(figsize=(6, 4))
-        self.ax = self.fig.add_subplot(111)
+def parse_args() -> argparse.Namespace:
+    """Parse command line arguments."""
+    import mngs
 
-    def teardown_method(self):
-        # Clean up after tests
-        plt.close(self.fig)
+    script_mode = mngs.gen.is_script()
+    parser = argparse.ArgumentParser(description="")
+    # parser.add_argument(
+    #     "--var",
+    #     "-v",
+    #     type=int,
+    #     choices=None,
+    #     default=1,
+    #     help="(default: %(default)s)",
+    # )
+    # parser.add_argument(
+    #     "--flag",
+    #     "-f",
+    #     action="store_true",
+    #     default=False,
+    #     help="(default: %%(default)s)",
+    # )
+    args = parser.parse_args()
+    mngs.str.printc(args, c="yellow")
+    return args
 
-    def test_basic_functionality(self):
-        # Get original position
-        original_bbox = self.ax.get_position()
-        original_width = original_bbox.width
-        original_height = original_bbox.height
-        original_center_x = original_bbox.x0 + original_width / 2
-        original_center_y = original_bbox.y0 + original_height / 2
 
-        # Extend width by 50% and keep height same
-        extended_ax = extend(self.ax, x_ratio=1.5, y_ratio=1.0)
-        new_bbox = extended_ax.get_position()
+def run_main() -> None:
+    """Initialize mngs framework, run main function, and cleanup."""
+    global CONFIG, CC, sys, plt
 
-        # Check that center point remains the same
-        new_center_x = new_bbox.x0 + new_bbox.width / 2
-        new_center_y = new_bbox.y0 + new_bbox.height / 2
-        assert np.isclose(new_center_x, original_center_x)
-        assert np.isclose(new_center_y, original_center_y)
+    import sys
 
-        # Check that width and height were scaled correctly
-        assert np.isclose(new_bbox.width, original_width * 1.5)
-        assert np.isclose(new_bbox.height, original_height * 1.0)
+    import matplotlib.pyplot as plt
+    import mngs
 
-    def test_shrink(self):
-        # Test shrinking the axes
-        original_bbox = self.ax.get_position()
-        original_width = original_bbox.width
-        original_height = original_bbox.height
+    args = parse_args()
 
-        # Shrink width and height by 50%
-        extended_ax = extend(self.ax, x_ratio=0.5, y_ratio=0.5)
-        new_bbox = extended_ax.get_position()
+    CONFIG, sys.stdout, sys.stderr, plt, CC = mngs.gen.start(
+        sys,
+        plt,
+        args=args,
+        file=__FILE__,
+        sdir_suffix=None,
+        verbose=False,
+        agg=True,
+    )
 
-        # Check that width and height were scaled correctly
-        assert np.isclose(new_bbox.width, original_width * 0.5)
-        assert np.isclose(new_bbox.height, original_height * 0.5)
+    exit_status = main(args)
 
-    def test_asymmetric_scaling(self):
-        # Test different scaling for width and height
-        original_bbox = self.ax.get_position()
-        original_width = original_bbox.width
-        original_height = original_bbox.height
+    mngs.gen.close(
+        CONFIG,
+        verbose=False,
+        notify=False,
+        message="",
+        exit_status=exit_status,
+    )
 
-        # Extend width but shrink height
-        extended_ax = extend(self.ax, x_ratio=2.0, y_ratio=0.75)
-        new_bbox = extended_ax.get_position()
-
-        # Check that width and height were scaled correctly
-        assert np.isclose(new_bbox.width, original_width * 2.0)
-        assert np.isclose(new_bbox.height, original_height * 0.75)
-
-    def test_edge_cases(self):
-        # Test with zero scaling (should be invalid but testing edge case)
-        with pytest.raises(Exception):
-            extend(self.ax, x_ratio=0, y_ratio=0)
-
-        # Test with default values (should keep size the same)
-        original_bbox = self.ax.get_position()
-        extended_ax = extend(self.ax)
-        new_bbox = extended_ax.get_position()
-
-        assert np.isclose(new_bbox.width, original_bbox.width)
-        assert np.isclose(new_bbox.height, original_bbox.height)
 
 if __name__ == "__main__":
-    import os
+    run_main()
 
-    import pytest
-
-    pytest.main([os.path.abspath(__file__)])
-
-# --------------------------------------------------------------------------------
-# Start of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/ax/_extend.py
-# --------------------------------------------------------------------------------
-# #!/usr/bin/env python3
-# 
-# 
-# def extend(ax, x_ratio=1.0, y_ratio=1.0):
-#     ## Original coordinates
-#     bbox = ax.get_position()
-#     left_orig = bbox.x0
-#     bottom_orig = bbox.y0
-#     width_orig = bbox.x1 - bbox.x0
-#     height_orig = bbox.y1 - bbox.y0
-#     g_orig = (left_orig + width_orig / 2.0, bottom_orig + height_orig / 2.0)
-# 
-#     ## Target coordinates
-#     g_tgt = g_orig
-#     width_tgt = width_orig * x_ratio
-#     height_tgt = height_orig * y_ratio
-#     left_tgt = g_tgt[0] - width_tgt / 2
-#     bottom_tgt = g_tgt[1] - height_tgt / 2
-# 
-#     ax.set_position(
-#         [
-#             left_tgt,
-#             bottom_tgt,
-#             width_tgt,
-#             height_tgt,
-#         ]
-#     )
-#     return ax
-# 
-# 
-# if __name__ == "__main__":
-#     import matplotlib.pyplot as plt
-# 
-#     fig, axes = plt.subplots(1, 2)
-#     ax = axes[1]
-#     ax = extend(ax, 0.75, 1.1)
-#     fig.show()
-
-# --------------------------------------------------------------------------------
-# End of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/ax/_extend.py
-# --------------------------------------------------------------------------------
+# EOF
