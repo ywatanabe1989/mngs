21c6acbbb4730291dc23a07c5b8238cfd02c38d3
LLEmacs <llemacs@llemacs.com>
2025-05-02 20:01:28 +1000

    Add  ColorLike to types module

--- src/mngs/types/__init__.py
+++ src/mngs/types/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-01 09:22:28 (ywatanabe)"
+# Timestamp: "2025-05-02 17:09:29 (ywatanabe)"
 # File: /home/ywatanabe/proj/mngs_repo/src/mngs/types/__init__.py
 # ----------------------------------------
 import os
@@ -13,5 +13,6 @@ __DIR__ = os.path.dirname(__FILE__)
 from typing import List, Tuple, Dict, Any, Union, Sequence, Literal, Optional, Iterable, Generator
 from ._ArrayLike import ArrayLike, is_array_like
 from ._is_listed_X import is_listed_X
+from ._ColorLike import ColorLike
 
 # EOF
