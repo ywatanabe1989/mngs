613db246f9da0cb4a19f454b1af21f8b5350cfc8
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-03 14:39:40 +1000

    Fix tests/mngs/plt/test__tpl.py

--- tests/mngs/plt/test__tpl.py
+++ tests/mngs/plt/test__tpl.py
@@ -50,7 +50,6 @@ def test_termplot_two_args(monkeypatch):
     termplot(x_values, y_values)
     assert dummy.plots[0] == (list(x_values), list(y_values))
 
-
 if __name__ == "__main__":
     import os
 
@@ -64,28 +63,28 @@ if __name__ == "__main__":
 # #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
 # # Time-stamp: "2024-03-31 11:58:28 (ywatanabe)"
-#
+# 
 # import numpy as np
 # import termplotlib as tpl
-#
-#
+# 
+# 
 # def termplot(*args):
 #     """
 #     Plots given y values against x using termplotlib, or plots a single y array against its indices if x is not provided.
-#
+# 
 #     Parameters:
 #     - *args: Accepts either one argument (y values) or two arguments (x and y values).
-#
+# 
 #     Returns:
 #     None. Displays the plot in the terminal.
 #     """
 #     if len(args) == 1:
 #         y = args[0]  # [REVISED]
 #         x = np.arange(len(y))
-#
+# 
 #     if len(args) == 2:
 #         x, y = args
-#
+# 
 #     fig = tpl.figure()
 #     fig.plot(x, y)
 #     fig.show()
@@ -93,5 +92,3 @@ if __name__ == "__main__":
 # --------------------------------------------------------------------------------
 # End of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/plt/_tpl.py
 # --------------------------------------------------------------------------------
-
-# EOF
