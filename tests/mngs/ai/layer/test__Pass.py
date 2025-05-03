8dca5c68bed2cfdcaa48ebac1b0524826d4ded4e
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-03 14:37:44 +1000

    Fix formatting in test__Pass.py

--- tests/mngs/ai/layer/test__Pass.py
+++ tests/mngs/ai/layer/test__Pass.py
@@ -25,22 +25,20 @@ if __name__ == "__main__":
 # # -*- coding: utf-8 -*-
 # # Time-stamp: "2024-11-20 00:29:47 (ywatanabe)"
 # # File: ./mngs_repo/src/mngs/ai/layer/_Pass.py
-#
+# 
 # THIS_FILE = "/home/ywatanabe/proj/mngs_repo/src/mngs/ai/layer/_Pass.py"
-#
+# 
 # import torch.nn as nn
-#
+# 
 # class Pass(nn.Module):
 #     def __init__(self,):
 #         super().__init__()
 #     def forward(self, x):
 #         return x
-#
-#
+# 
+# 
 # # EOF
 
 # --------------------------------------------------------------------------------
 # End of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/ai/layer/_Pass.py
 # --------------------------------------------------------------------------------
-
-# EOF
