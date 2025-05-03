8a10e323469c158f54a94f3c68fc5989cd792f04
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-03 14:37:45 +1000

    Fix formatting in test_wait_key.py

--- tests/mngs/etc/test_wait_key.py
+++ tests/mngs/etc/test_wait_key.py
@@ -24,32 +24,32 @@ if __name__ == "__main__":
 # #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
 # # Time-stamp: "2023-03-24 23:13:32 (ywatanabe)"
-#
+# 
 # import readchar
 # import time
 # import multiprocessing
-#
+# 
 # def wait_key(p):
 #     key = "x"
 #     while key != "q":
 #         key = readchar.readchar()
-#         print(key)
+#         print(key)        
 #     print("q was pressed.")
 #     p.terminate()
 #     # event.set()
 #     # raise Exception
-#
-#
+# 
+# 
 # def count():
 #     counter = 0
 #     while True:
 #         print(counter)
 #         time.sleep(1)
 #         counter += 1
-#
+# 
 # if __name__ == "__main__":
 #     p1 = multiprocessing.Process(target=count)
-#
+#     
 #     p1.start()
 #     waitKey(p1)
 #     print("aaa")
@@ -57,5 +57,3 @@ if __name__ == "__main__":
 # --------------------------------------------------------------------------------
 # End of Source Code from: /home/ywatanabe/proj/_mngs_repo/src/mngs/etc/wait_key.py
 # --------------------------------------------------------------------------------
-
-# EOF
