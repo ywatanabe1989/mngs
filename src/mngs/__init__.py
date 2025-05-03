946ecd52e88b168328963e630b0593a34d8495d4
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-03 16:31:50 +1000

    Bump mngs version to 1.11.0

--- src/mngs/__init__.py
+++ src/mngs/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-01 09:22:55 (ywatanabe)"
+# Timestamp: "2025-05-03 16:31:08 (ywatanabe)"
 # File: /home/ywatanabe/proj/mngs_repo/src/mngs/__init__.py
 # ----------------------------------------
 import os
@@ -60,7 +60,7 @@ from . import dev
 # # Usage: python -m mngs print_config
 
 __copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
-__version__ = "1.10.1"
+__version__ = "1.11.0"
 __license__ = "MIT"
 __author__ = "ywatanabe1989"
 __author_email__ = "ywatanabe@alumni.u-tokyo.ac.jp"
